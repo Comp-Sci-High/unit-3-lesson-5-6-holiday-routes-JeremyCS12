ac66e7a28820f4acc867c7ff7f7a30722278a6bd
JeremyCS12 <jeremy.ortiz26@compscihigh.org>
2025-12-18 20:02:34 +0000

    christmas game/project

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const app = express()
 app.use(express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  // Redirect home to the themed Christmas route for this assignment
+  res.redirect('/christmas');
+})
+
+app.get("/christmas", (req, res) => {
+  res.sendFile(__dirname + "/public/christmas.html");
+})
+
+app.get("/mr-castro", (req, res) => {
+  res.sendFile(__dirname + "/public/mr-castro.html");
+})
+
+app.get("/christmas-facts", (req, res) => {
+  res.sendFile(__dirname + "/public/christmas-facts.html");
 })
 
 app.listen(3000, () => {
