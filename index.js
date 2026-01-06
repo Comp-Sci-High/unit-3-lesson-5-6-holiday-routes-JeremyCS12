8db627d7293244272838e7a2679e34ef06554c73
JeremyCS12 <jeremy.ortiz26@compscihigh.org>
2026-01-06 14:13:32 +0000

    Added the comments for homework

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express = require("express")
-const app = express()
 
-app.use(express.static(__dirname + "/public"))
+const express = require("express") //Import the express library
+const app = express() //Create an express application
 
-app.get("/", (req, res) => {
+app.use(express.static(__dirname + "/public")) //Serve static files from the public directory
+
+app.get("/", (req, res) => { //Home route
   // Redirect home to the themed Christmas route for this assignment
-  res.redirect('/christmas');
+  res.redirect('/christmas'); // Redirect to /christmas
 })
 
-app.get("/christmas", (req, res) => {
-  res.sendFile(__dirname + "/public/christmas.html");
+app.get("/christmas", (req, res) => { //Christmas route
+  res.sendFile(__dirname + "/public/christmas.html"); //Send the christmas.html file
 })
 
-app.get("/mr-castro", (req, res) => {
-  res.sendFile(__dirname + "/public/mr-castro.html");
+app.get("/mr-castro", (req, res) => { //Mr. Castro route
+  res.sendFile(__dirname + "/public/mr-castro.html"); //Send the mr-castro.html file
 })
 
-app.get("/christmas-facts", (req, res) => {
-  res.sendFile(__dirname + "/public/christmas-facts.html");
+app.get("/christmas-facts", (req, res) => { //Christmas Facts route
+  res.sendFile(__dirname + "/public/christmas-facts.html"); //Send the christmas-facts.html file
 })
 
-app.listen(3000, () => {
-  console.log(`Holiday Server is Running!`)
+app.listen(3000, () => { //Start the server on port 3000
+  console.log(`Holiday Server is Running!`) //Log a message to the console
 });
